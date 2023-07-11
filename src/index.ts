29e4cdfbcfe0c84cb06faee46f5c30b308a5db4d
tuna-tuna <bouningen13@gmail.com>
2023-07-11 22:22:30 +0900

    completed scheduled task

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { GatewayIntentBits } from "discord.js";
-import { SapphireClient } from "@sapphire/framework";
-import { Database } from "./utils/database";
+import { SapphireClient, Events } from "@sapphire/framework";
 import * as dotenv from "dotenv";
+import { Database } from "./utils/database";
 import { ImageGeneration } from "./utils/image";
+import { updateLastMatchInfo } from "./utils/task";
 dotenv.config();
 
 const client = new SapphireClient({
@@ -16,5 +17,11 @@ export const database = new Database(db_url);
 await database.init();
 export const image_generation = new ImageGeneration();
 await image_generation.init();
+
+client.once(Events.ClientReady, () => {
+  setInterval(() => {
+    updateLastMatchInfo();
+  }, 300000);
+});
 
 client.login(process.env["DISCORD_TOKEN"]);
--- src/utils/task.ts
+++ src/utils/task.ts
@@ -5,51 +5,35 @@ import {
   UnknownAPIError,
 } from "./error";
 import { getLastCompetitiveMatchFromPUUID } from "./fetch";
-import { ScheduledTask } from "@sapphire/plugin-scheduled-tasks";
 
-export class IntervalTask extends ScheduledTask {
-  public constructor(
-    context: ScheduledTask.Context,
-    options: ScheduledTask.Options
-  ) {
-    super(context, {
-      ...options,
-      interval: 300_000,
-    });
-  }
-
-  public run = async () => {
-    await this.updateLastMatchInfo();
-  };
-
-  private updateLastMatchInfo = async () => {
-    const docs = await database.getAllCollections();
-    await Promise.all(
-      docs.map((doc) => {
-        doc.riot_accounts.map(async (riot_account) => {
-          try {
-            const discord_id = doc.discord_id;
-            const puuid = riot_account.puuid;
-            const record_match_id = riot_account.last_match_id;
-            const { match_id, match_date } =
-              await getLastCompetitiveMatchFromPUUID(puuid, "ap");
-            if (record_match_id !== match_id) {
-              await database.updateMatchInfo(
-                discord_id,
-                puuid,
-                match_id,
-                match_date
-              );
-            }
-          } catch (error) {
-            if (error instanceof ResourceNotFoundError) {
-            } else if (error instanceof UnknownAPIError) {
-            } else if (error instanceof DatabaseTransactionError) {
-            } else {
-            }
+export const updateLastMatchInfo = async () => {
+  console.log("Updating match ids...");
+  const docs = await database.getAllCollections();
+  await Promise.all(
+    docs.map((doc) => {
+      doc.riot_accounts.map(async (riot_account) => {
+        try {
+          const discord_id = doc.discord_id;
+          const puuid = riot_account.puuid;
+          const record_match_id = riot_account.last_match_id;
+          const { match_id, match_date } =
+            await getLastCompetitiveMatchFromPUUID(puuid, "ap");
+          if (record_match_id !== match_id) {
+            await database.updateMatchInfo(
+              discord_id,
+              puuid,
+              match_id,
+              match_date
+            );
           }
-        });
-      })
-    );
-  };
-}
+        } catch (error) {
+          if (error instanceof ResourceNotFoundError) {
+          } else if (error instanceof UnknownAPIError) {
+          } else if (error instanceof DatabaseTransactionError) {
+          } else {
+          }
+        }
+      });
+    })
+  );
+};
